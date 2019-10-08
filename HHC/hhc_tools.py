022aebca44a9be35ca22dd5898e7520486dc706c
jhcollins <jhcollins12@gmail.com>
2019-10-08 13:00:09 -0500

    adding the main hhc_tools run file to start building off of

---
+++ HHC/hhc_tools.py
@@ -0,0 +1,13 @@
+#This is the main program file for the HHC-Tools GUI#
+
+#import background python modules#
+import numpy as np
+import tkinter as tk
+import matplotlib.pyplot as plt
+
+#-------------------------#
+
+#import our HHC-Tools submodules#
+#import overtopping as ot
+#import precipitation as pcip
+
