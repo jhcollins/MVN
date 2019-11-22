2203c2b42d2ace1d0b8d2bca7beac9c6e622884f
dfertitta <davidfertitta@gmail.com>
2019-11-22 16:02:35 -0600

    Added option to input today's measured stage. Made script that downloads NWS forecast more robust.

--- HHC/river_forecast/MS_fcst/get_NWS_river_forecast.py
+++ HHC/river_forecast/MS_fcst/get_NWS_river_forecast.py
@@ -4,7 +4,7 @@
 # ## Gets River Forecast Data (5- and 28- Day) from NWS
 # https://www.weather.gov/lmrfc/obsfcst_mississippi
 
-# In[ ]:
+# In[2]:
 
 
 def get_nws_fcst(file_out5,file_out28,today_date):
@@ -25,8 +25,15 @@ def get_nws_fcst(file_out5,file_out28,today_date):
     # Parse html data from website
     nws_html = BeautifulSoup(nws.text, "html.parser") # NOTE: data is pre-formatted <pre>
 
+    ######### find 5- and 28-day forecasts
+    ind_5day=['']*len(nws_html.findAll('a'))
+    ind_28day=['']*len(nws_html.findAll('a'))
+    for i in range(len(nws_html.findAll('a'))):
+        ind_5day[i] = 'Lower Mississippi River 5-day Forecast Summary' in str(nws_html.findAll('a')[i])
+        ind_28day[i] = 'Lower Mississippi River 28-day Forecast Summary' in str(nws_html.findAll('a')[i]) 
+
     ###################### Get 5-day forecast data ##########################
-    dum = nws_html.findAll('a')[118]
+    dum = nws_html.findAll('a')[int(np.where(np.array(ind_5day)==True)[0])]#[118]
     pred_5day = dum['href']
     html_5day = BeautifulSoup(req.get(pred_5day).text, "html.parser")
     text_5day = html_5day.find('pre').contents[0]
@@ -83,14 +90,19 @@ def get_nws_fcst(file_out5,file_out28,today_date):
     # Put list into dataframe
     fcst_5day = pd.DataFrame(list_5day)
 
+    # get today's stage at Red River Landing
+    array_5day = np.array(fcst_5day)
+    i_rrl, j_rrl = np.where(array_5day=='RED RIVER LNDG')
+    i_stg, j_stg = np.where(array_5day=='STG')
 
+    rrl_stg_today = np.float(array_5day[i_rrl,j_stg])
 
     # Pause so website doesn't think we're a hacker (probably more relevant for loops)
     time.sleep(1)
 
     ##################### Get 28-day forecast data ############################
-    dum = nws_html.findAll('a')[119]
-    pred_28day = dum['href']
+    dum1 = nws_html.findAll('a')[int(np.where(np.array(ind_28day)==True)[0])]#[119]
+    pred_28day = dum1['href']
     html_28day = BeautifulSoup(req.get(pred_28day).text, "html.parser")
     text_28day = html_28day.findAll('pre')[0]
     # Separate the rows specified in the data by \n
@@ -135,7 +147,7 @@ def get_nws_fcst(file_out5,file_out28,today_date):
 
     ###############################  Stages for New Orleans ##########################
     fcst28 = np.array(fcst_28day)
-    k, l = np.where(fcst28=="NORL1") # used k and l as indices since i,j had been used
+    k, l = np.where(fcst28=="NORL1") # find NO fcst; used k and l as indices since i,j had been used
 
     dates_fcst21_NO = [datetime.strptime(x, "%m-%d-%y").date() for x in fcst28[int(k)+1:int(k)+22,0]]
 
@@ -154,5 +166,11 @@ def get_nws_fcst(file_out5,file_out28,today_date):
     dates_fcst21_RR = [datetime.strptime(x, "%m-%d-%y").date() for x in fcst28[int(k)+1:int(k)+22,0]]
     fcst_21_RR = np.float64(fcst28[int(k)+1:int(k)+21+1,int(l)])
 
-    return time_pred21, dates_pred21, fcst_21_NO, fcst_21_RR
+    return time_pred21, dates_pred21, fcst_21_NO, fcst_21_RR, rrl_stg_today
+
+
+# In[ ]:
+
+
+
 
